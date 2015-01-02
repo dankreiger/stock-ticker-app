f86e7650b9c6edc54f4e2dbff42fc30d468a7ff9
Dan Kreiger <dan@dankreiger.com>
2015-01-02 02:55:00 -0500

    stock ticker implemented

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,7 @@ class StocksController < ApplicationController
   def index
   	# search = params[:search] || "book"
   	search = params[:search] || "alibaba"
+  	@company = search
   	# response = HTTParty.get("http://www.amazon.com/s/ref=sr_st_review-rank?keywords=#{search}")
   	response = HTTParty.get("http://www.reuters.com/search?blob=#{search}")
   	# dom = Nokogiri::HTML(response.body)
